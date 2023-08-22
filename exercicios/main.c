045c8f82878fccc16eb516f552fe467e8fe0befc
guifzy <guifrxx@gmail.com>
2023-08-22 18:11:34 -0300

    1.0

---
+++ exercicios/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    int x;
+    printf("\nDigita: ");
+    scanf("%d", &x);
+    printf("Valor de x: %d", x);
+
+    return 0;
+}
+
