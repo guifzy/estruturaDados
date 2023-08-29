46a34c3202e871ad545e49fd08728115eb8ba422
guifzy <guifrxx@gmail.com>
2023-08-29 17:58:21 -0300

    1.3

--- exercicios/main.c
+++ exercicios/main.c
@@ -1,62 +1,81 @@
 #include <stdio.h>
+#include <string.h> 
 
-int main()
-{
-	FILE* arqv;
-	char nome_arquivo[50], conteudo[100];
-		
-	printf("Entre com o nome do arquivo, o ponto e a extensão: ");
-	scanf("%s", nome_arquivo);
+typedef struct pessoa{
+    char nome[50];
+    int idade;
+    float altura;
+    char telefone[10];
+} pessoa;
 
-	arqv = fopen(nome_arquivo, "r");
+pessoa amigos[5];
+const int MAX = 5;
 
-	if (arqv == NULL)
-	{
-		printf("Erro ao abrir arquivo.");
-		return 1;
-	}
+int main() {
+    int i = 0, achou = 0;
+    char num[50];
+    FILE* arqv;
+    FILE* format;
+    arqv = fopen("dados.txt", "w");
 
-	while (fgets(conteudo, sizeof(conteudo), arqv) != NULL)
-	{
-		printf("%s", conteudo);
-	}
-
-<<<<<<< HEAD
-	fclose(arqv);
-	return 0;
-=======
-    FILE* fh_input;
-    fh_input = fopen("io.txt", "r");
-
-    int finput = 0;
-    int lines = 0;
-    int numbers[100];
-
-    
-    while (fscanf(fh_input, "%d", &finput) != EOF)
-    {
-        
-        numbers[lines] = finput;
-        printf("file line %d: %d\n", lines + 1, finput);
-        lines++;
+    if (arqv == NULL) {
+        printf("Erro ao abrir arquivo");
+        return 1;
     }
 
-    int total = 0;
-    for (int i = 0; i < lines; i++)
-        total = i;
-    printf("Average: %d\n", total + 1);
+    for (i = 0; i < MAX; i++) {
+        achou = 0;
 
-    fclose(fh_input);
- /*
-    fh_input = fopen("in.txt", "r");
+        printf("\nDigite o telefone do amigo %d: ", i + 1);
+        scanf("%9s", amigos[i].telefone);
+        getchar();
 
+        for (int j = 0; j < i; j++) {
+            if (strcmp(amigos[i].telefone, amigos[j].telefone) == 0) {
+                achou = 1;
+                break;
+            }
+        }
+        if (achou == 1) {
+            printf("Esse telefone ja foi cadastrado");
+            fclose(arqv);
+            return 1;
+        }
+        else {
+            printf("\nDigite o nome do amigo %d: ", i + 1);
+            fgets(amigos[i].nome, sizeof(amigos[i].nome), stdin);
 
-    fgets(buffer, 256, fh_input);
-    printf("Buffer: %s\n", buffer);
+            printf("\nDigite a idade do amigo %d: ", i + 1);
+            scanf("%d", &amigos[i].idade);
 
-    fclose(fh_input);
-    */
+            printf("\nDigite a altura do amigo %d: ", i + 1);
+            scanf("%f", &amigos[i].altura);
+            getchar();
+
+            fwrite(&amigos[i], sizeof(pessoa), 1, arqv);
+        }
+    }
+
+    fclose(arqv);
+
+    pessoa amigo[5];
+
+    arqv = fopen("dados.txt", "r");
+    format = fopen("amigosFormatados.txt", "w");
+
+    if (arqv == NULL || format == NULL) {
+        printf("Erro ao abrir arquivo");
+        return 1;
+    }
+
+    for (i = 0; i < MAX; i++) {
+        fread(&amigo[i], sizeof(pessoa), 1, arqv);
+        fprintf(format, "[%s] tem [%d] anos e [%.2f] de altura.\nTel.: [%s].\n",
+            amigo[i].nome, amigo[i].idade, amigo[i].altura, amigo[i].telefone);
+    }
+
+    fclose(arqv);
+    fclose(format);
 
     return 0;
->>>>>>> 7ab03322a29844b015adff978639f53059af656d
 }
